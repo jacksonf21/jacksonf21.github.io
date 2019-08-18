dd4ecfd21674ab888a25c5be547b17f8a344c612
Jackson Fung <jacksonf21@hotmail.com>
2019-08-18 18:01:23 +0000

    api2

--- public/script/app.js
+++ public/script/app.js
@@ -1,14 +1,30 @@
-const request = require('request');
 
 $(document).ready(() => {
 
-  console.log('hello');
+  const escape = str => {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-  $('#api button').click(() => {
-
-    request('https://www.reddit.com/r/heroesofthestorm/.json', (err, data) => {
-      if (err) console.log(err);
+  const promise1 = () => {
+    return new Promise((resolve, reject) => {
+      $.getJSON('https://www.reddit.com/r/heroesofthestorm/.json', (data) => {
+        resolve(data);
+      });
     });
+  };
+
+  const appendData = (data) => {
+    console.log(data.data);
+    let html = `<p>${escape(data)}</p>`;
+    $('#apiDump').append(html);
+  };
+
+  $('#api').submit((event) => {
+    event.preventDefault();
+    promise1()
+      .then((data) => appendData(data));
   });
 
 });
